4655ed6d360b17ead81d1e950b5e7b01a4e8e968
vagrant <vagrant@homestead>
2018-04-02 07:42:42 +0000

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@
 */
 
 
-// Route::get('/',"StaticPagesController@home");
+Route::get('/',"StaticPagesController@home")->name('home');
 Route::get("/help","StaticPagesController@help")->name('help');
 Route::get("/about","StaticPagesController@about")->name("about");
 // Route::get("/help",["as"=>"help",'uses'=>"StaticPagesController@help"]);
-Route::match(array('get','post'), "/","StaticPagesController@home")->name("home");
+// Route::match(array('get','post'), "/","StaticPagesController@home")->name("home");
 Route::get("signup","UsersController@create")->name("signup");
