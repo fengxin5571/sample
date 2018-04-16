d8c60b71296c5e4253c9b6d99bec05eb2fcbe743
vagrant <vagrant@homestead>
2018-04-16 07:25:14 +0000

    fengxin test

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -14,7 +14,7 @@
             <label for="name">名称：</label>
             <input type="text" name="name" class="form-control" value="{{ old('name') }}">
           </div>
-
+            
           <div class="form-group">
             <label for="email">邮箱：</label>
             <input type="text" name="email" class="form-control" value="{{ old('email') }}">
