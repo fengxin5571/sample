0437228bdd5ed25e0f84d4909a325a3b7b98587d
vagrant <vagrant@homestead>
2018-04-16 08:01:51 +0000

    fengxintest

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,7 +27,7 @@ class SessionsController extends Controller
                 return redirect()->intended(route("users.show",[Auth::user()]));
             }else{
                 Auth::logout();
-                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。'.Auth::user()->activation_token);
                 return redirect("/");
             }
             
